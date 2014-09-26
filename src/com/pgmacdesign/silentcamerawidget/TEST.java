d32b235b1d8bfd6ed70f78de245ae9a64ad96c67
PGMacDesign <pgmacdesign@gmail.com>
2014-09-26 01:36:22 -0700

    Woot. I cannot type an exclamation point, but I would if I could. Finally got the adjust the megapixel method to work perfectly. It takes the highest resolution available, which is what I originally set out to do with this project.

--- src/com/pgmacdesign/silentcamerawidget/TEST.java
+++ src/com/pgmacdesign/silentcamerawidget/TEST.java
@@ -11,56 +11,6 @@ import android.widget.ImageView;
 
 public class TEST extends Activity {
 
-	   private Camera cameraObject;
-	   private ShowCamera showCamera;
-	   private ImageView pic;
-	   public static Camera isCameraAvailiable(){
-	      Camera object = null;
-	      try {
-	         object = Camera.open(); 
-	      }
-	      catch (Exception e){
-	      }
-	      return object; 
-	   }
-
-	   private PictureCallback capturedIt = new PictureCallback() {
-
-	      @Override
-	      public void onPictureTaken(byte[] data, Camera camera) {
-
-	      Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
-	      if(bitmap==null){
-	         Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
-	      }
-	      else
-	      {
-	         Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
-	      }
-	      cameraObject.release();
-	   }
-	};
-
-	   @Override
-	   protected void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      setContentView(R.layout.activity_main);
-	      pic = (ImageView)findViewById(R.id.imageView1);
-	      cameraObject = isCameraAvailiable();
-	      showCamera = new ShowCamera(this, cameraObject);
-	      FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-	      preview.addView(showCamera);
-	   }
-	   public void snapIt(View view){
-	      cameraObject.takePicture(null, null, capturedIt);
-	   }
-
-	   @Override
-	   public boolean onCreateOptionsMenu(Menu menu) {
-	      getMenuInflater().inflate(R.menu.main, menu);
-	      return true;
-	   }
-	}
 
 
 }
--- src/com/pgmacdesign/silentcamerawidget/TakePhoto.java
+++ src/com/pgmacdesign/silentcamerawidget/TakePhoto.java
@@ -1,10 +1,13 @@
 package com.pgmacdesign.silentcamerawidget;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.util.Log;
@@ -100,6 +103,59 @@ public class TakePhoto extends Activity {
 		}
 	}
 	
+	//Sets the resolution on the photo to be the highest possible resolution
+   	private void setCameraResolution() {
+   		
+   		//Parameters object to get the parameters of the camera
+   		Camera.Parameters params = camera.getParameters();
+   		
+   		//Check what resolutions are supported by your camera
+	   	List<Size> resolution_size_choices = params.getSupportedPictureSizes();
+   		
+	   	//Make a list type variable (of type size). The temp one is for the for loop
+   		Size size, sizeTemp;
+   		
+   		//These variables will help to set the highest resolution possible
+   		int highest_height = 0;
+   		int highest_width = 0;
+   		int height1 = 0;
+		int width1 = 0;
+   		
+   		//Run a for loop that cycles through the resolution choices and lists them 
+   		for (int i = 0; i < resolution_size_choices.size(); i++){
+   			
+   			//Read the resolution from the size list
+   			sizeTemp = resolution_size_choices.get(i);
+   			//Determine the height at the current list position
+   			height1 = sizeTemp.height;
+   			//Determine the width at the current list position
+   			width1 = sizeTemp.width;
+   			
+   			//Set the height to the highest one thus far
+   			if (height1 > highest_height){
+   				highest_height = height1;
+   			}
+   	
+   			//Set the width to the highest one thus far
+   			if (width1 > highest_width){
+   				highest_width = width1;
+   			}   			
+   		}
+	   	
+	   	//Set the parameters to match the highest Megapixel available
+	   	params.setPictureSize(highest_width, highest_height);
+	   	camera.setParameters(params);   		
+	   	
+	   	//Log the Megapixels from the results of the loop
+   		//Gets the resolution (Height x Width)
+   		int aResolution = highest_height * highest_width;
+   		//For the calculation of Megapixels. Not really needed, but fun to have anyway
+   		int megaPixels = aResolution / 1024000;
+   		//Log it
+   		L.m("Resolution = " + aResolution);
+   		L.m("Megapixels = " + megaPixels);
+	   	
+   	}
 
 	
 	
