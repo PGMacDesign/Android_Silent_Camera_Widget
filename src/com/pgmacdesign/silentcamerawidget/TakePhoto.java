892282c94942250809fe7aa3cc2ec680c9a2e1e5
PGMacDesign <pgmacdesign@gmail.com>
2014-10-15 11:24:07 -0700

    Updates to photo taking class

--- src/com/pgmacdesign/silentcamerawidget/TakePhoto.java
+++ src/com/pgmacdesign/silentcamerawidget/TakePhoto.java
@@ -3,40 +3,63 @@ package com.pgmacdesign.silentcamerawidget;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
+import android.view.TextureView.SurfaceTextureListener;
 import android.widget.FrameLayout;
 
-public class TakePhoto extends Activity {
+public class TakePhoto extends Activity implements SurfaceTextureListener {
 
-	  private int cameraId = 0;	
-	  private static final int IMAGE_CAPTURE = 102;
-	  private Camera cameraObject, myCamera;
-	  private ShowCamera showCamera;
+	private int cameraId = 0;	
+	private static final int IMAGE_CAPTURE = 102;
+	private Camera cameraObject, myCamera;
+	private ShowCamera showCamera;
+    	
+	private TextureView mTextureView;
 	
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		///setContentView(R.layout.second_activity);
-		L.m("Testing Line 27");
-		sendRecognizeIntent();
+		
+		//Creating the texture view and setting it equal to the content view to handle the overlay
+		mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+        mTextureView.setVisibility(View.GONE); 
+        setContentView(mTextureView);
+        L.m("Line 41");
+        
+        cameraObject = isCameraAvailiable();
+        
+        L.m("Line 45");
+		
+        takeThePhotoPlease();
+		
+        
+        
+        
+        //L.m("Testing Line 27");
+		//sendRecognizeIntent();
 		
 		//cameraObject = isCameraAvailiable(); //Initialize camera
-		L.m("Testing Line 31");
+		//L.m("Testing Line 31");
 		//setCameraResolution(); //Sets resolution to max on pictures
-		L.m("Testing Line 36");
+		//L.m("Testing Line 36");
 		
-		takePictureNoPreview(this);
-		L.m("Testing line 39");
+		//takePictureNoPreview(this);
+		//L.m("Testing line 39");
 		
 
 	}
@@ -54,8 +77,18 @@ public class TakePhoto extends Activity {
 	//This method calls other respective methods to take the photo via the startActivityForResult()
 	private void takeThePhotoPlease() {
 
-		
-		
+		//Shut off the shutter sound
+		silenceTheWorld();
+		L.m("Line 81");
+		//Set the camera parameters (IE resolution)
+		setCameraResolution();
+		L.m("Line 84");
+		//Take the picture
+		actuallyActivateCamera();
+		L.m("Line 87");
+		//Restore the sound
+		restoreSound();
+		L.m("Line 90");
 	}
 	
 	public void takePictureNoPreview(Context context){
@@ -131,19 +164,22 @@ public class TakePhoto extends Activity {
 	private void actuallyActivateCamera() {
 	    
 		//Preview window
-	    showCamera = new ShowCamera(this, cameraObject);
-	    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-	    preview.addView(showCamera);
+	    //showCamera = new ShowCamera(this, cameraObject);
+	    //FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+	    //preview.addView(showCamera);
 	    
    		//Take the photo!
+		L.m("Camera - Line 171");
    		cameraObject.takePicture(null, null, new PhotoHandler(getApplicationContext()));
+   		L.m("Camera - Line 173");
    		
 
    		//Popup a dialog after 3 seconds to release the camera
    		Handler handler = new Handler(); 
    		handler.postDelayed(new Runnable() { 
  	        public void run() { 
-	 	   		finish();
+
+	 	   		//finish(); //Removed for now
  	        } 
  	    }, (1000*5));
    		
@@ -232,12 +268,26 @@ public class TakePhoto extends Activity {
    		
    	}
 
+	//onResume
+	public void onResume() {
+		super.onResume();
+
+		if (cameraObject == null) {
+			cameraObject=Camera.open();
+			L.m("Camera open in onResume");
+		}
+
+		//startPreview(); Need to add this in 
+	}
+	
    	//onPause()
 	protected void onPause() {
 		if (cameraObject != null) {
 			cameraObject.release();
 			cameraObject = null;
-			L.m("Camera has been released");
+			L.m("Camera has been released (onPause)");
+			//cameraObject.stopPreview();
+			//L.m("Preview has been released (onPause)");
 		}
 		super.onPause();
 	}
@@ -247,7 +297,9 @@ public class TakePhoto extends Activity {
 		if (cameraObject != null) {
 			cameraObject.release();
 			cameraObject = null;
-			L.m("Camera has been released");
+			L.m("Camera has been released (onDestroy)");
+			//cameraObject.stopPreview();
+			//L.m("Preview has been released (onDestroy)");
 		}
 		super.onDestroy();
 	}
@@ -274,5 +326,39 @@ public class TakePhoto extends Activity {
 		L.m("Testing Line 37");		
 		//takeThePhotoPlease(); 
 		
+	}
+
+	//Setup the Camera
+	public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+		
+		//cameraObject = Camera.open();
+
+        Camera.Size previewSize = cameraObject.getParameters().getPreviewSize();
+        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
+                previewSize.width, previewSize.height, Gravity.CENTER));
+
+        try {
+        	cameraObject.setPreviewTexture(surface);
+        } catch (IOException t) {
+        }
+
+        cameraObject.startPreview();
+	}
+
+	//Android camera will handle this method
+	public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+		//IGNORE THIS METHOD, must be included as it is implemented, but can be ignored as the camera handles it
+	}
+
+	//Destroy camera objects
+	public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+		//cameraObject.stopPreview();
+		//cameraObject.release();
+        return true;
+	}
+
+	//Update texture here when there is something to change
+	public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+		//Update the view if need be here
 	}
 }
